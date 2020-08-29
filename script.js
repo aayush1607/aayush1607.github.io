7ee0902388c5b0ffdb2c435b072145c95ad5c5b1
aayush1607 <auc1607@gmail.com>
2020-08-29 14:33:55 +0530

    ui changes

--- script.js
+++ script.js
@@ -38,3 +38,22 @@ myskills.addEventListener('click',function(){
 contact.addEventListener('click',function(){
     mobileNav.classList.remove('open');
 });
+
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
